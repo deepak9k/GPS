3e18861c33c118684a8f6d480bf2cddce5214b88
Deepak <dpkkumar1555@gmail.com>
2018-03-15 12:38:52 +0530

    location parma change

--- agps/agps.py
+++ agps/agps.py
@@ -6,9 +6,9 @@ from flask import Flask, request, session, url_for, redirect, render_template, \
      g, flash, _app_ctx_stack, jsonify
 # create application
 
-app = Flask('agps')
-app.config.from_object(__name__)
-app.config.from_envvar('WEB_SETTINGS', silent=True)
+
+#app.config.from_object(__name__)
+#app.config.from_envvar('WEB_SETTINGS', silent=True)
 
 
 #Flask object
@@ -37,33 +37,40 @@ def get_location(bus_id):
     error=None
     north=0
     east=0
+    temp=0
     for task in record:
         if task['id']==bus_id:
-           north = task['north']
-           east =  task['east']
+           temp= task['north']
+           temp=temp/100
+           north=temp//1
+           temp=temp-north
+           north+=temp*1.667
 
-           print("done if")
-    print("done for")
+           temp = task['east']
+           temp = temp / 100
+           east = temp // 1
+           temp = temp - east
+           east += temp*1.667
+
+
     return render_template('map.html', north=north, east=east)
 
 @app.route('/5466/<int:bus_id>', methods=['POST'])
 def set_location(bus_id):
 
     flag=0
-
     for task in record:
         if task['id']==bus_id:
             flag=1
-            task['north']=request.form['north']
-            task['east']=request.form['east']
-    if flag==1:
+            task['north']=request.json['north']
+            task['east']=request.json['east']
+    if flag==0:
         task = {
-        'id': request.form['bus_id'],
-        'north': request.form['north'],
-        'east': request.form['east']
+        'id': request.json['id'],
+        'north': request.json['north'],
+        'east': request.json['east']
             }
         record.append(task)
-
     return jsonify({'added':bus_id}), 201
 
 
